a76ba61955769c890546da039472a1d9f6665d09
dongwook <hsw50108@gmail.com>
2024-05-06 03:13:22 +0900

    add : [backend] - BaseTimeEntity

--- backend/src/main/java/com/example/backend/entity/Customer.java
+++ backend/src/main/java/com/example/backend/entity/Customer.java
@@ -1,4 +1,4 @@
-package com.example.backend.domain;
+package com.example.backend.entity;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -6,7 +6,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -20,7 +19,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name="customer")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Customer {
+public class Customer extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
--- backend/src/main/java/com/example/backend/entity/User.java
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -1,15 +1,12 @@
-package com.example.backend.domain;
+package com.example.backend.entity;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "users")
 @NoArgsConstructor(access = PROTECTED)
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
